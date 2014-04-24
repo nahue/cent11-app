d21c18341d8f764e68988f76c5686d6818c74f79
Nahuel Chaves <nahuel.chaves@gmail.com>
2014-04-24 12:38:25 -0300

    fix imágenes subidas

--- cent11/urls.py
+++ cent11/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, include, url
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
@@ -11,4 +10,4 @@ urlpatterns = patterns('',
     # url(r'^$', 'cent11.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
-) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- configs/base.py
+++ configs/base.py
@@ -43,7 +43,7 @@ MEDIA_URL = '/media/'
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
 STATIC_ROOT = BASE_ROOT + '/static/'
-
+MEDIA_ROOT = BASE_ROOT + '/media/'
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
